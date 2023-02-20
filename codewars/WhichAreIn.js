948743cce0b0b4450bbfb14d2500f0456f86dab7
ploshchanski <93840857+ploshchanski@users.noreply.github.com>
2023-02-21 01:05:59 +0300

    ref

--- codewars/WhichAreIn.js
+++ codewars/WhichAreIn.js
@@ -1,12 +1,10 @@
-function inArray(array1,array2){
-    let newArray = [];
-    array1.forEach((item)=>{
-        if(array2.join(',').includes(item)){
-            newArray.push(item);
-        }
-    })
-    console.log(newArray);
-      return newArray.sort();
+function inArray(array1, array2) {
+  let newArray = [];
+  array1.forEach((item) => {
+    if (array2.join(",").includes(item)) {
+      newArray.push(item);
     }
-
-
+  });
+  console.log(newArray);
+  return newArray.sort();
+}
